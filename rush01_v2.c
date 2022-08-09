94931ce11a161a55927f2a38664355d34541bd98
EudaldV98 <janeudald98@gmail.com>
2022-08-09 12:27:46 +0200

    Done rush01_v2

---
+++ rush01_v2.c
@@ -0,0 +1,61 @@
+void	ft_putchar(char c);
+
+void	print_top_down(int width, char left, char middle, char right)
+{
+	int	i;
+
+	ft_putchar(left);
+	i = 0;
+	while (++i <= width - 2)
+		ft_putchar(middle);
+	ft_putchar(right);
+	ft_putchar('\n');
+}
+
+void	case_width(int height, char top, char filler, char bottom)
+{
+	int i;
+
+	ft_putchar(top);
+	i = 0;
+	while (++i <= height - 2)
+	{
+		ft_putchar('\n');
+		ft_putchar(filler);
+	}
+	ft_putchar('\n');
+	ft_putchar(bottom);
+	ft_putchar('\n');
+}
+
+void	case_height(int width, char left, char filler, char right)
+{
+	int i;
+
+	ft_putchar(left);
+	i = 0;
+	while (++i <= width - 2)
+		ft_putchar(filler);
+	ft_putchar(right);
+	ft_putchar('\n');
+}
+
+void	rush(int width, int height)
+{
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return ;
+	if (width == 1)
+		case_width(height, '/', '*', '\\');
+	else if (height == 1)
+		case_height(width, '/', '*', '\\');
+	else
+	{
+		print_top_down(width, '/', '*', '\\');
+		i = 0;
+		while (++i <= height - 2)
+			case_height(width, '*', ' ', '*');
+		print_top_down(width, '\\', '*', '/');
+	}
+}
